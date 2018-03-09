7eed11eeaeb371c1a4584f122cd695bbc9ac73b7
Solomon Xie <solomonxiewise@gmail.com>
2018-03-09 21:03:58 +0800

    Update from Solomon-mba.local

--- mathjax_github_issues.user.js
+++ mathjax_github_issues.user.js
@@ -1,37 +1,22 @@
 // ==UserScript==
-// @name           MathJax in Wikipedia
+// @name           MathJax
 // @namespace      http://www.mathjax.org/
 // @description    Insert MathJax into Wikipedia pages
-// @include        http://en.wikipedia.org/wiki/*
 // @include        https://github.com/*/*/issues/*
 // ==/UserScript==
 
 if ((window.unsafeWindow == null ? window : unsafeWindow).MathJax == null) {
-  //
-  //  Replace the images with MathJax scripts of type math/tex
-  //
-  var images = document.getElementsByTagName('img');
-  for (var i = images.length - 1; i >= 0; i--) {
-    var img = images[i];
-    if (img.className === "tex") {
-      var script = document.createElement("script"); script.type = "math/tex";
-      //if (window.opera) {script.innerHTML = img.alt} else {script.text = img.alt}
-      img.parentNode.replaceChild(script,img);
-    }
+  if ((document.getElementsByTagName("math").length > 0) ||
+      (document.getElementsByTagNameNS == null ? false :
+      (document.getElementsByTagNameNS("http://www.w3.org/1998/Math/MathML","math").length > 0))) {
+    var script = document.createElement("script");
+    script.src = "https://raw.githubusercontent.com/solomonxie/user_content_media/master/MathJax.js";  // put your URL here
+    var config = 'MathJax.Hub.Config({' +
+                   'extensions:["mml2jax.js"],' +
+                   'jax:["input/MathML","output/HTML-CSS"]' +
+                 '});' +
+                 'MathJax.Hub.Startup.onload()';
+    if (window.opera) {script.innerHTML = config} else {script.text = config}
+    document.getElementsByTagName("head")[0].appendChild(script);
   }
-  //
-  //  Load MathJax and have it process the page
-  //
-  var script = document.createElement("script");
-  script.src = "https://raw.githubusercontent.com/solomonxie/user_content_media/master/MathJax.js";  // put your URL here
-  var config = 'MathJax.Hub.Config({' +
-                 'config: ["MMLorHTML.js"],' +
-                 'extensions:["TeX/noErrors.js","TeX/noUndefined.js",' +
-                             '"TeX/AMSmath.js","TeX/AMSsymbols.js"],' +
-                 'jax:["input/TeX"]' +
-               '});' +
-               'MathJax.Hub.Startup.onload()';
-  //if (window.opera) {script.innerHTML = config} else {script.text = config}
-  document.getElementsByTagName("head")[0].appendChild(script);
 }
-
