ac456b952e3b46aaba1af9bcf8b56ae6ca7b6f8b
Solomon Xie <solomonxiewise@gmail.com>
2018-03-09 21:19:14 +0800

    Update from Solomon-mba.local

--- mathjax_github_issues.user.js
+++ mathjax_github_issues.user.js
@@ -11,11 +11,7 @@ if ((window.unsafeWindow == null ? window : unsafeWindow).MathJax == null) {
       (document.getElementsByTagNameNS("http://www.w3.org/1998/Math/MathML","math").length > 0))) {
     var script = document.createElement("script");
     script.src = "https://raw.githubusercontent.com/solomonxie/user_content_media/master/MathJax.js";  // put your URL here
-    var config = 'MathJax.Hub.Config({' +
-                   'extensions:["mml2jax.js"],' +
-                   'jax:["input/MathML","output/HTML-CSS"]' +
-                 '});' +
-                 'MathJax.Hub.Startup.onload()';
+    var config = 'MathJax.Hub.Config({' + 'extensions:["mml2jax.js"],' + 'jax:["input/MathML","output/HTML-CSS"]' + '});' + 'MathJax.Hub.Startup.onload()';
     if (window.opera) {script.innerHTML = config} else {script.text = config}
     document.getElementsByTagName("head")[0].appendChild(script);
   }
