53bbfee8ebfaa6b022216e1a812e58822af8b0fc
Solomon Xie <solomonxiewise@gmail.com>
2018-03-10 21:15:52 +0800

    edited

--- mathjax_github_issues.user.js
+++ mathjax_github_issues.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name           MathJax
+// @name           MathJax for Github
 // @namespace      http://www.mathjax.org/
-// @description    Insert MathJax into Wikipedia pages
+// @description    Insert MathJax into Github comment pages
 // @include        https://github.com/*/*/issues/*
 // ==/UserScript==
-
+//
+/*
 if ((window.unsafeWindow == null ? window : unsafeWindow).MathJax == null) {
   if ((document.getElementsByTagName("math").length > 0) ||
       (document.getElementsByTagNameNS == null ? false :
@@ -12,7 +13,15 @@ if ((window.unsafeWindow == null ? window : unsafeWindow).MathJax == null) {
     var script = document.createElement("script");
     script.src = "https://raw.githubusercontent.com/solomonxie/user_content_media/master/MathJax.js";  // put your URL here
     var config = 'MathJax.Hub.Config({' + 'extensions:["mml2jax.js"],' + 'jax:["input/MathML","output/HTML-CSS"]' + '});' + 'MathJax.Hub.Startup.onload()';
-    if (window.opera) {script.innerHTML = config} else {script.text = config}
+    if (window.opera) {script.innerHTML = config;} else {script.text = config;}
     document.getElementsByTagName("head")[0].appendChild(script);
   }
 }
+*/
+
+
+var script = document.createElement("script");
+script.src = "https://raw.githubusercontent.com/solomonxie/user_content_media/master/MathJax.js";  // put your URL here
+var config = 'MathJax.Hub.Config({' + 'extensions:["mml2jax.js"],' + 'jax:["input/MathML","output/HTML-CSS"]' + '});' + 'MathJax.Hub.Startup.onload()';
+if (window.opera) {script.innerHTML = config;} else {script.text = config;}
+document.getElementsByTagName("head")[0].appendChild(script);
